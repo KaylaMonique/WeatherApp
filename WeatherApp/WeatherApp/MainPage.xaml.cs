3d3871d763e5c10352f0b83df02e78591f67f87a
KaylaMonique <102808151+KaylaMonique@users.noreply.github.com>
2022-06-13 15:15:31 +0200

    updated mainpage.xaml.cs

--- WeatherApp/WeatherApp/MainPage.xaml.cs
+++ WeatherApp/WeatherApp/MainPage.xaml.cs
@@ -1,10 +1,12 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
 using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
+using Xamarin.Essentials;
 using Xamarin.Forms;
 using static WeatherApp.API_Info;
 
@@ -12,70 +14,35 @@ namespace WeatherApp
 {
     public partial class MainPage : ContentPage
     {
-        private double _temperature;
-
-        public double Temperature
-        {
-            get { return _temperature; }
-            set
-            {
-                _temperature = value;
-
-                OnPropertyChanged();
-            }
-        }
-
-
-        private Main _weather;
-
-        public Main WeatherInformation
-        {
-            get { return _weather; }
-            set
-            {
-                _weather = value;
-
-                OnPropertyChanged();
-            }
-        }
-
-        public openWeatherData AllWeather
-        {
-            get;
-            set;
-        }
-
         public MainPage()
         {
             InitializeComponent();
-            this.BindingContext = AllWeather;
         }
+        
+        private async Task<openWeatherData> GetWeatherData()
+        {
+            var location = await Geolocation.GetLocationAsync();
+            var latitude = location.Latitude;
+            var longitude = location.Longitude;
 
-        public class Weather
-        {
-            public string Temp { get; set; }
-            public string Date { get; set; }
-            public string Icon { get; set; }
-        }
-        private async Task GetWeatherData()
-        {
-            HttpClient client = new HttpClient();
+            var client = new HttpClient();
+
             client.DefaultRequestHeaders.Add("Accept", "application/json");
-            string response = await client.GetStringAsync("https://api.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=27c5cd68ffd4b9cc8e225cd37435b08b}");
 
-            var weatherData = Newtonsoft.Json.JsonConvert.DeserializeObject<openWeatherData>(response);
+            var response = await client.GetStringAsync("https://api.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=27c5cd68ffd4b9cc8e225cd37435b08b");
 
-            if (weatherData != null)
-            {
-                AllWeather = weatherData;
-            }
+            var weatherData = JsonConvert.DeserializeObject<openWeatherData>(response);
+
+            return weatherData;
         }
 
         protected async override void OnAppearing()
         {
             base.OnAppearing();
 
-            await GetWeatherData();
+            var data = await GetWeatherData();
+
+            BindingContext = data;
         }
     }
 }
